dd600269e692449a0c10288f486d9809dd64a993
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-04-25 16:00:47 +0000

    Added Healthfilter

---
+++ src/main/java/uk/ac/ebi/fgpt/webapp/HealthFilter.java
@@ -0,0 +1,32 @@
+package uk.ac.ebi.fgpt.webapp;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * A health check filter that complies with EBI E.S. guidelines about how to filter health requests to your webapp to
+ * prevent excessive logging of automated checks.
+ * <p/>
+ * Web applications deployed at one of the EBI London Datacentres are monitored by a load balancer.  This load balancer
+ * sends a request every three seconds, and the datacentre tomcats are set up to log requests in access log files (using
+ * a valve).  By setting this HealthFilter in your webapp, health check requests will be filtered and the log files will
+ * not be overwhelmed.
+ * <p/>
+ * For more information, see <a href="http://wwwint.ebi.ac.uk/es/web-administration/Public/load-balancer-monitor>http://wwwint.ebi.ac.uk/es/web-administration/Public/load-balancer-monitor</a>
+ *
+ * @author Tony Burdett
+ * @date 13/10/11
+ */
+public class HealthFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        servletRequest.setAttribute("health", true);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+    }
+}
