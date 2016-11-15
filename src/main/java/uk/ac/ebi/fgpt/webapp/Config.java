32d6e086d7e08aa0a1c46b8597d748b115a0448b
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-15 15:54:05 +0000

    add jdbctemplate details for accession database

--- src/main/java/uk/ac/ebi/fgpt/webapp/Config.java
+++ src/main/java/uk/ac/ebi/fgpt/webapp/Config.java
@@ -1,14 +1,15 @@
 package uk.ac.ebi.fgpt.webapp;
 
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import uk.ac.ebi.fgpt.sampletab.Accessioner;
 
@@ -17,15 +18,22 @@ public class Config {
     
 	private Logger log = LoggerFactory.getLogger(getClass());
 
-	public Config() {
-	}    
+	public Config() {}
 	
-    @Bean
-    @Autowired
-    public Accessioner getAccessioner() throws NamingException {
+		
+	@Bean
+	public JdbcTemplate getJdbcTemplate() {
 		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
-		DataSource dataSource = dataSourceLookup.getDataSource("java:comp/env/jdbc/accessionDB");
-    	return new Accessioner(dataSource);
+		DataSource accessionDataSource = dataSourceLookup.getDataSource("java:comp/env/jdbc/accessionDB");
+		return new JdbcTemplate(accessionDataSource);		
+	}
+		
+	
+	@Bean
+    public Accessioner getAccessioner() {
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		DataSource accessionDataSource = dataSourceLookup.getDataSource("java:comp/env/jdbc/accessionDB");
+    	return new Accessioner(accessionDataSource);
     }
     
     @Bean
